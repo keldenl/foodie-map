643fc8c0f796a6a10ced05ca2accfc8760a3e3f8
Kelden Lin <keldenlin@gmail.com>
2017-05-26 16:35:17 -0700

    Added levels(?)

--- scripts/mapLocation.R
+++ scripts/mapLocation.R
@@ -22,6 +22,7 @@ loc <- "c800 occidental ave s, seattle, wa" # Location you're searching for
 long_lat <- as.numeric(geocode(loc))
 loc <- revgeocode(long_lat, output="more") # View() to see more about the location
 zip <- loc$postal_code
+open_now <- FALSE
 
 # df stores all the restaurant information
 df <- data.frame(name=NULL, id=NULL, lat=NULL, long=NULL, loc=NULL, phone=NULL, rating=NULL, price=NULL,
@@ -31,7 +32,7 @@ df <- data.frame(name=NULL, id=NULL, lat=NULL, long=NULL, loc=NULL, phone=NULL, 
 for(j in seq(0, total-1, 50)) {
   offset <- j # Offset to the next set of 50 businesses
   query <- paste0("https://api.yelp.com/v3/businesses/search?location=", zip, "&limit=", limit,
-                  "&offset=", offset, "&term=restaurant")
+                  "&offset=", offset, "&term=restaurant", "&open_now=", open_now)
   data <- GET(url=query, add_headers(Authorization="bearer O8RZ1gWMOz120LusXeF_s_HhkLlwLQBrd9_SLV9r9ltR8zJdHY9g_mFDtZGyX7EMa2XkHFTRbFDo_8ZhRxlWX1apsp-4gSW5U0hlIOnuwQceLTlmQCKX99nnDAMmWXYx"))
   data <- fromJSON(content(data,type="text"))
   
@@ -69,12 +70,16 @@ map1 <- get_map(location = location, source = "google", zoom = mapNormal)
 # Make a map with restaurants as points on it
 maps <- ggmap(map1) +
   # Add restaurant markers
-  geom_point(data=df, aes(name=name, rating=rating, x=long, y=lat), color="green") + 
+  geom_point(data=df, aes(name=name, rating=rating, reviews=rev_count, price=price, 
+                          x=long, y=lat), color="green") + 
+  geom_density2d(data=df, aes(color=rating, x=long, y=lat)) +
+  scale_fill_gradient(low = "green", high = "red", 
+                      guide = FALSE) +
   coord_fixed(1.3) +
   
   # Labels
   labs(title="Restaurants near your location")
-maps <- ggplotly(maps, tooltip = c("name", "rating"), dynamicTicks = FALSE, width = 500)
+maps <- ggplotly(maps, tooltip = c("name", "rating", "reviews", "price"), dynamicTicks = FALSE, width = 500)
 
 # Check out the map!
 maps
