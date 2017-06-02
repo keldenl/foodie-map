7e865b8d4bbad007febd19a3eeeacccd014d2690
Kelden Lin <keldenlin@gmail.com>
2017-06-01 17:34:59 -0700

    Fixed NULL latitude/longitude Yelp locations, requesting large amounts of restaurants no longer crashs the app. Cleaned up a lot of (repetitive) code and added comments everywhere.

--- scripts/mapLocation.R
+++ scripts/mapLocation.R
@@ -12,134 +12,107 @@ library(ggplot2) #install.packages("ggplot2", type = "source")
 library(plotly)
 
 # Set Variables
-limit <- 50 # Unchanged
+limit <- 50 # Yelp only allows 50 restaurants per request (Unchanged)
 
+## Returns the plotly map/graph based on the dataframe provided
 generateGraph <- function(df, long_lat, scope, cat, min_rat, price_range, heat) {
-  # Category filter
-  if (!is.null(cat)){ # default selects everything
-    df <- filter(df, categories %in% cat) 
-  }
-
-  # Rating filter
+  # Category, Zoom, Rating, Price filters (Dynamic: reactive, updates instantly)
+  if (!is.null(cat)){ df <- filter(df, categories %in% cat) } # Default (empty) selects all categories
+  mapNormal <- 15 # Default arbitrary zoom
+  mapZoom <- mapNormal - (2 - scope)
   df <- filter(df, rating >= min_rat)
-  
-  # Price filter
   df <- filter(df, as.numeric(price) %in% price_range)
   
-  # Zoom filter
-  mapNormal <- 15
-  mapZoom <- mapNormal - (2 - scope)
-  
-  # Get a map for the location entered
+  # Get the map for the location entered (Google Maps)
   location <- c(lon = long_lat[1], lat = long_lat[2])
   map1 <- get_map(location = location, source = "google", zoom = mapZoom)
   
-  # Make a map with restaurants as points on i
-  ## PRICE
+  # Add Markers to the map generated based on the Heatmap option 
+  # HEATMAP OPTION 1: PRICE
   if (heat == "price") {
     maps <- ggmap(map1) +
       geom_point(data=df, aes(name=name, categories=categories, rating=rating, reviews=rev_count, price=price,
                               x=long, y=lat, color=as.numeric(price)), size=4, alpha=.8) +
-      scale_colour_gradient(low="yellow",high="red") +
-      # Custom style for the graph
-      theme(axis.title.x=element_blank(),
-            axis.text.x=element_blank(),
-            axis.ticks.x=element_blank(),
-            axis.title.y=element_blank(),
-            axis.text.y=element_blank(),
-            axis.ticks.y=element_blank(),
-            legend.background = element_rect(fill="rgba(0, 0, 0, 0"),
-            legend.title = element_text(color="white"),
-            legend.text = element_text(color="white"))+
-      labs(color="Price (1=$)")
+      labs(color="Price (1=$, 4=$$$$)")
   } 
-  
-  ## REVIEWS
+  # HEATMAP OPTION 2: REVIEWS
   else if (heat == "review.count") {
     df$review_500 <- "> 500"
     df$review_500[df$rev_count <= 500] <- NA
-    
     maps <- ggmap(map1) +
-      # Plot (subsets) twice to remove outliers (> 500 review counts)
+      # Plot (subsets of df) twice to remove outliers (> 500 review counts are categorized together)
       geom_point(data=subset(df, rev_count <= 500), aes(name=name, categories=categories, rating=rating, reviews=rev_count, 
                                                         price=price, x=long, y=lat, color=rev_count), size=4, alpha=.8) +
       geom_point(data=subset(df, rev_count > 500), aes(name=name, categories=categories, rating=rating, reviews=rev_count, 
                                                        price=price, x=long, y=lat, fill=review_500), size=4, alpha=.8) +
-      scale_colour_gradient(low="yellow",high="red") +
-      # Custom style for the graph
-      theme(axis.title.x=element_blank(),
-            axis.text.x=element_blank(),
-            axis.ticks.x=element_blank(),
-            axis.title.y=element_blank(),
-            axis.text.y=element_blank(),
-            axis.ticks.y=element_blank(),
-            legend.background = element_rect(fill="rgba(0, 0, 0, 0"),
-            legend.title = element_text(color="white"),
-            legend.text = element_text(color="white"))+
       labs(color="# of Reviews", fill="")
   } 
-  
-  ## RATINGS
+  # HEATMAP OPTION 3: RATINGS
   else {
     maps <- ggmap(map1) +
       geom_point(data=df, aes(name=name, categories=categories, rating=rating, reviews=rev_count, price=price,
                               x=long, y=lat, color=rating), size=4, alpha=.8) +
-      scale_colour_gradient(low="yellow",high="red") +
-      # Custom style for the graph
-      theme(axis.title.x=element_blank(),
-            axis.text.x=element_blank(),
-            axis.ticks.x=element_blank(),
-            axis.title.y=element_blank(),
-            axis.text.y=element_blank(),
-            axis.ticks.y=element_blank(),
-            legend.background = element_rect(fill="rgba(0, 0, 0, 0"),
-            legend.title = element_text(color="white"),
-            legend.text = element_text(color="white"))+
-      labs(color="Rating")
+      labs(color="Ratings")
   }
   
-  # Make ggplot
+  # Custom style for the graphs
+  maps <- maps + 
+    scale_colour_gradient(low="yellow",high="red") +
+    theme(axis.title.x=element_blank(),
+          axis.text.x=element_blank(),
+          axis.ticks.x=element_blank(),
+          axis.title.y=element_blank(),
+          axis.text.y=element_blank(),
+          axis.ticks.y=element_blank(),
+          legend.background = element_rect(fill="rgba(0, 0, 0, 0"),
+          legend.title = element_text(color="white"),
+          legend.text = element_text(color="white"))
+  
+  # Convert of ggplotly (interactive)
   maps <- ggplotly(maps, tooltip = c("name", "categories", "rating", "reviews", "price"), 
                    dynamicTicks = FALSE, width = 700, height = 580)
   return (maps)
 }
 
-getProg <- function() {
-  return (prog)
-}
-
+# Returns a vector list of restaurant categories in the location
 getCategory <- function(df, location, total){
-  # Changable Variables
   category <- df %>% distinct(categories)
   return(as.vector(category))
 }
 
-
+## Returns the dataframe of interest from Yelp's API
 returnDf <- function(zip, open_now, total) {
-  # df stores all the restaurant information
+  # Df stores all the restaurant information
   df <- data.frame(name=NULL, id=NULL, lat=NULL, long=NULL, loc=NULL, phone=NULL, rating=NULL, price=NULL,
-                   rev_count=NULL, img=NULL, url=NULL, categories=NULL)
+                   rev_count=NULL, categories=NULL)
   
-  # Loop through and add restaurants to the dataframe df
+  # Make multiple requests and add restaurants to the main dataframe df
   for(j in seq(0, total-1, 50)) {
-    offset <- j # Offset to the next set of 50 businesses
+    offset <- j # Offsets to the next set of 50 businesses
+    
+    # GET request from Yelp API
     query <- paste0("https://api.yelp.com/v3/businesses/search?location=", zip, "&limit=", limit,
                     "&offset=", offset, "&term=restaurant", "&open_now=", open_now)
     data <- GET(url=query, add_headers(Authorization="bearer O8RZ1gWMOz120LusXeF_s_HhkLlwLQBrd9_SLV9r9ltR8zJdHY9g_mFDtZGyX7EMa2XkHFTRbFDo_8ZhRxlWX1apsp-4gSW5U0hlIOnuwQceLTlmQCKX99nnDAMmWXYx"))
     data <- fromJSON(content(data,type="text",encoding="ISO-8859-1"))
+    
+    # Loops through the 50 (or less) restaurants returned from GET request
     for (i in 1:length(data$businesses)) {
-      if(is.null(data$businesses[[i]])) { break } #  If out of bounds
+      if(length(data$businesses) == 0) { break } #  If retrieved ALL restaurants, break from loop
+      
       curr <- data$businesses[[i]]
-      if(is.null(curr$price)) { curr$price <- NA}
-      curr_df <- data.frame(name=curr$name, id=curr$id, lat=curr$coordinates$latitude, long=curr$coordinates$longitude, 
-                            loc=curr$location$display_address, phone=curr$phone, rating=curr$rating, price=curr$price,
-                            rev_count=curr$review_count, img=curr$image_url, url=curr$url, categories=curr$categories[[1]]$title)
-      df <- rbind(df, curr_df)
-      #prog <- (i+j)/(total)*100
-      #print(paste0(prog, "%"))
+      if(is.null(curr$price)) { curr$price <- NA} # If no price is provided, make it NA (to avoid errors)
+      
+      # Create dataframe: if no coordinates are provided, skip it. (can't plot)
+      if (!is.null(curr$coordinates$latitude)) {
+        curr_df <- data.frame(name=curr$name, id=curr$id, lat=curr$coordinates$latitude, long=curr$coordinates$longitude, 
+                              loc=curr$location$display_address, phone=curr$phone, rating=curr$rating, price=curr$price,
+                              rev_count=curr$review_count, categories=curr$categories[[1]]$title)
+        df <- rbind(df, curr_df) # Add this restaurant row to the accumulated restaurant list
+      }
     }
   }
+  
   df <- distinct(df, name, .keep_all = TRUE)
   return (df)
 }
-
--- server.R
+++ server.R
@@ -1,54 +1,33 @@
 library(shiny)
 source("./scripts/mapLocation.R")
 
+# Shiny Server START
 shinyServer(function(input, output, session) {
+  # "Surprise me" enters the location of the UW Ave to search
   observeEvent(input$surprise, {
     updateTextInput(session, inputId = "loc", value = "University way, Seattle WA")
   })
   
   # Initial search function 
   observeEvent(input$submitSearch, {
-    # Process Address Entered
-    long_lat <- as.numeric(geocode(input$loc))
-    loc <- revgeocode(long_lat, output="more") # View() to see more about the location
-    zip <- loc$postal_code
-    
-    df <- returnDf(zip, input$open.now, input$num.restaurants)
-    output$returnAmount <- renderText({ paste("Returned", nrow(df), "restaurants in", input$loc) })
-    
-    # Graph
-    output$graph <- renderPlotly({
-      generateGraph(df, long_lat, input$map.zoom, input$category, input$min.rating, 
-                    input$price.range, input$heatmap.type) %>% 
-        layout(plot_bgcolor='rgba(0, 0, 0, 0)') %>% 
-        layout(paper_bgcolor='rgba(0, 0, 0, 0)')
-    })
-    
+    updateMap()
     updateTabsetPanel(session, inputId = "tabs", selected = "graph")
-    updateSelectInput(session, inputId = "category", 
-                      choices = getCategory(df, input$loc, input$num.restaurants))
-  })
-  
-  observeEvent(input$main, {
-    updateTabsetPanel(session, inputId = "tabs", selected = "main")
   })
   
   # Static filters (re-fetch, similar to initial search)
   observeEvent(input$filter, {
-    # Process Address Entered
-    long_lat <- as.numeric(geocode(input$loc))
-    loc <- revgeocode(long_lat, output="more") # View() to see more about the location
-    zip <- loc$postal_code
-    
-    df <- returnDf(zip, input$open.now, input$num.restaurants)
-    output$returnAmount <- renderText({paste("Returned", nrow(df), "restaurants in", input$loc)})
-    # Graph
-    output$graph <- renderPlotly({
-      generateGraph(df, long_lat, input$map.zoom, input$category, input$min.rating, 
-                    input$price.range, input$heatmap.type) %>% 
-        layout(plot_bgcolor='rgba(0, 0, 0, 0)') %>% 
-        layout(paper_bgcolor='rgba(0, 0, 0, 0)')
-    })
+    updateMap()
+  })
+  
+  # "Search again", goes back to main tab
+  observeEvent(input$main, {
+    updateTabsetPanel(session, inputId = "tabs", selected = "main")
+  })
+  
+  # Validate Ratings filter
+  numbers <- reactive({
+    validate(need(is.numeric(input$min.rating) && input$min.rating >= 0 && input$min.rating <= 5, 
+                  "Please input a valid rating"))
   })
   
   # Set up all the widgets
@@ -56,13 +35,30 @@ shinyServer(function(input, output, session) {
   output$min.rating <- renderPrint({input$min.rating})
   output$price.range <- renderPrint({input$price.range})
   output$map.zoom <- renderPrint({input$map.zoom})
-  
-  # Validate Ratings filter
-  numbers <- reactive({
-    validate(
-      need(is.numeric(input$min.rating) && input$min.rating >= 0 && input$min.rating <= 5, 
-           "Please input a valid rating")
-    )
-  })
   output$ratingVal <- renderPrint({ numbers() }) 
+  
+  # Function that returns the map/graph
+  updateMap <- function() {
+    # Process Address Entered
+    long_lat <- as.numeric(geocode(input$loc)) # Convert address to Longitude & Latitude
+    loc <- revgeocode(long_lat, output="more") # View() to see more about the location
+    zip <- loc$postal_code
+    
+    # Get Dataframe and information about it
+    df <- returnDf(zip, input$open.now, input$num.restaurants)
+    output$returnAmount <- renderText({ paste("Returned", nrow(df), "restaurants in", input$loc) })
+    
+    # Plot Graph
+    output$graph <- renderPlotly({
+      generateGraph(df, long_lat, input$map.zoom, input$category, input$min.rating, 
+                    input$price.range, input$heatmap.type) %>% 
+        layout(plot_bgcolor='rgba(0, 0, 0, 0)') %>% 
+        layout(paper_bgcolor='rgba(0, 0, 0, 0)')
+    })
+    
+    # Update Categories options displayed
+    updateSelectInput(session, inputId = "category", 
+                      choices = getCategory(df, input$loc, input$num.restaurants))
+  }
+  
 })
